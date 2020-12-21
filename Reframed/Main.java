74eee22d2f8143781c6f1ed0fad2ec2a37930fa2
Justin Aurand <justin.aurand@creditly.com>
2020-12-20 22:26:25 -0500

    add styles

--- Reframed/Main.java
+++ Reframed/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String args[]) {
-        Style style = new Style();
+        ArrayList<Style> styles = new ArrayList<Style>() {
+            {
+                add(new Style("Chic", 0.10f, 0.18f, 0.25f, 0.32f));
+                add(new Style("Deco", 0.12f, 0.20f, 0.28f, 0.35f));
+                add(new Style("Modern", 0.13f, 0.25f, 0.33f, 0.45f));
+                add(new Style("Chic", 0.15f, 0.28f, 0.40f, 0.47f));
+                add(new Style("Chic", 0.16f, 0.30f, 0.45f, 0.50f));
+            }
+        };
     }
 }
--- Reframed/Style.java
+++ Reframed/Style.java
@@ -1,2 +1,33 @@
 public class Style {
+    public String name;
+    public float halfInchWidthPrice;
+    public float oneInchWidthPrice;
+    public float oneAndAHalfInchWidthPrice;
+    public float twoInchWidthPrice;
+
+    public Style(
+        String name,
+        float halfInchWidthPrice,
+        float oneInchWidthPrice,
+        float oneAndAHalfInchWidthPrice,
+        float twoInchWidthPrice
+    ) {
+        this.name = name;
+        this.halfInchWidthPrice = halfInchWidthPrice;
+        this.oneInchWidthPrice = oneInchWidthPrice;
+        this.oneAndAHalfInchWidthPrice = oneAndAHalfInchWidthPrice;
+        this.twoInchWidthPrice = twoInchWidthPrice;
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(name + ", ");
+        builder.append(halfInchWidthPrice + ", ");
+        builder.append(oneInchWidthPrice + ", ");
+        builder.append(oneAndAHalfInchWidthPrice + ", ");
+        builder.append(twoInchWidthPrice);
+
+        return builder.toString();
+    }
 }
